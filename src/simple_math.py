6b3ea79a5f4535ac07bd6a34b89e8e1ee0bab34a
Romain <romainB1222@gmail.com>
2026-01-19 16:25:11 +0100

    ci: add pylint linting step

--- src/simple_math.py
+++ src/simple_math.py
@@ -1,9 +1,15 @@
+"""Simple mathematical operations."""
+
+
 class SimpleMath:
+    """Utility class for simple math operations."""
+
     @staticmethod
     def addition(a, b):
+        """Return the addition of a and b."""
         return a + b
 
     @staticmethod
     def soustraction(a, b):
+        """Return the subtraction of b from a."""
         return a - b
-
